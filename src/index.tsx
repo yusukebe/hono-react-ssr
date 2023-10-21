83adadfbbd26d69608d5d6d9dc69c3bea311c12f
Yusuke Wada <yusuke@kamawada.com>
2023-10-22 07:05:15 +0900

    use renderer

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,32 @@
 import { Hono } from 'hono'
+import { createMiddleware } from 'hono/factory'
 import { renderToString } from 'react-dom/server'
+
+declare module 'hono' {
+  interface ContextRenderer {
+    (node: React.ReactNode): Response
+  }
+}
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.html(
-    renderToString(
-      <html>
-        <body>
-          <h1>Hello</h1>
-        </body>
-      </html>
+const renderer = createMiddleware(async (c, next) => {
+  c.setRenderer((content) => {
+    return c.html(
+      renderToString(
+        <html>
+          <body>{content}</body>
+        </html>
+      )
     )
-  )
+  })
+  await next()
+})
+
+app.get('*', renderer)
+
+app.get('/', (c) => {
+  return c.render(<h1>Hello!</h1>)
 })
 
 export default app
